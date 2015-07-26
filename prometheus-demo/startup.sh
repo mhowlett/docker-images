1333c31db266de917ef72fc82e9c97646c735b4b
Matt Howlett <matt@mhowlett.com>
2015-07-26 22:39:28 +0700

    removed prometheus

--- prometheus-demo/startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-/root/client_golang/example_random -listen-address=:8081 &
-/root/client_golang/example_random -listen-address=:8082 &
-/root/client_golang/example_random -listen-address=:8083 &
-
-/root/prometheus/prometheus -config.file=/root/prometheus.yml
